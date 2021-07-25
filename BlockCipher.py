3016390ad5319e2f228047ebe69612d3269b8167
PatMcN7 <65691549+PatMcN7@users.noreply.github.com>
2021-07-25 13:58:49 -0400

    Added shifting blocks and un-shifting them in the cipher.

--- BlockCipher.py
+++ BlockCipher.py
@@ -23,7 +23,27 @@ def rebuild_message(message_list, block_size = 4):
             message += chr(number)
     return message
 
-#
+#Shifting the blocks
+def apply_rotate(message_list, key, block_size = 4):
+    cipher_list = []
+    bit_max = block_size * 8
+    for i in range(len(message_list)):
+        chunk = message_list
+        carry = chunk % 2 ** key
+        carry = carry <<  (bit_max - key)
+        cipher = (chunk >> key) + carry
+        cipher_list.append(cipher)
+    return cipher_list
 
+def undo_shift(cipher_list, key, block_size=4):
+    message_list = []
+    bit_max = block_size * 8
+    for i in range(len(cipher_list)):
+        chunk = cipher_list[i]
+        carry = chunk % (2 ** (bit_max - key))
+        carry = carry << key
+        number = (chunk >> (bit_max - key)) + carry
+        message_list.append(number)
+    return message_list
 
 
